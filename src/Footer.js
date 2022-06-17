e20f1c657edd1a0560d04498a2bcc653daf53cc9
nickick <hong.nick@gmail.com>
2022-06-17 15:22:31 -0400

    Clean up footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,33 +1,7 @@
-import {
-  Box, Container, Typography,
-} from '@mui/material';
-import NextLink from 'next/link';
+import { Container, Typography } from '@mui/material';
 import { useContext } from 'react';
 import { LoadedContext } from './LoadedContextProvider';
 import { entranceAnimationDuration, fadeFromBelow } from './utils/animations';
-
-const sitemapLinks = [
-  {
-    text: 'Home',
-    link: '/',
-  },
-  {
-    text: 'Publications',
-    link: '/publications',
-  },
-  {
-    text: 'Gallery',
-    link: '/gallery',
-  },
-  {
-    text: 'About',
-    link: '/about',
-  },
-  {
-    text: 'Contact',
-    link: '/contact',
-  },
-];
 
 export default function Footer() {
   const { animationDelay } = useContext(LoadedContext);
@@ -41,107 +15,11 @@ export default function Footer() {
         zIndex: 3,
       }}
     >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: {
-            xs: 'column',
-            md: 'row',
-          },
-          width: '100%',
-        }}
+      <Typography
+        variant="h1"
       >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            flex: 2,
-          }}
-        >
-          <Typography
-            variant="h3"
-          >
-            To the moon and never back.
-          </Typography>
-          <Typography
-            variant="overline"
-          >
-            Email
-          </Typography>
-          <Typography
-            variant="body"
-          >
-            <NextLink
-              href="mailto: driftershoots@gmail.com"
-            >
-              driftershoots@gmail.com
-            </NextLink>
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            flex: 1,
-          }}
-        >
-          <Typography
-            variant="overline"
-            sx={{
-              mt: {
-                xs: 1,
-                md: 0,
-              },
-            }}
-          >
-            Sitemap
-          </Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: {
-                xs: 'column',
-                md: 'row',
-              },
-            }}
-          >
-            {
-              sitemapLinks.map(({ text, link }) => (
-                <Typography
-                  variant="body"
-                  key={text}
-                  sx={{
-                    m: 0.5,
-                  }}
-                >
-                  <NextLink
-                    href={link}
-                  >
-                    {text}
-                  </NextLink>
-                </Typography>
-              ))
-            }
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            flex: 1,
-          }}
-        >
-          <Typography
-            variant="overline"
-          >
-            <NextLink
-              href="/privacy-policy"
-            >
-              Privacy Policy
-            </NextLink>
-          </Typography>
-        </Box>
-      </Box>
+        Footer
+      </Typography>
     </Container>
   );
 }
