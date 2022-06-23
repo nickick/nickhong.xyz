906b33ed7ddc4f07b3229b83fdab94f4db554527
nickick <hong.nick@gmail.com>
2022-06-23 15:49:04 -0400

    Fix more project data

--- src/Projects/project-data.js
+++ src/Projects/project-data.js
@@ -3,10 +3,10 @@ const projects = [
     name: 'FirstDayOut',
     href: 'https://firstdayout.driftershoots.com',
     description: [
-      "FirstDayOut is <a href='https://twitter.com/driftershoots' target='_blank'>@driftershoots</a> Open Edition NFT mint. Celebrating the 1 year anniversary of Drift's release from wrongful incarceration, the NFT drop saw over 10,000 mints.",
-      'This raised $6.7 million, with over $1 million going to support the Bail Bond Fund, making this the most commercially successful photograph in history.',
-      'I was part of the technical implementation team, responsible for the ethers.js static dapp to mint.',
-      'In addition, this NFT has a built-in toggle to flip between 2 image assets per NFT, a first of its kind.',
+      "FirstDayOut is <a href='https://twitter.com/driftershoots' target='_blank'>@driftershoots</a>&apos;s Open Edition NFT mint. Celebrating the 1 year anniversary of Drift's release from wrongful incarceration, the NFT drop saw over 10,000 mints.",
+      "I was part of the technical implementation team, responsible for the ethers.js conneted Dapp to mint. <a href='https://twitter.com/andrewjiang' target='_blank'>@andrewjiang</a> wrote the smart contract, and <a href='https://twitter.com/jeffreyraefan' target='_blank'>@jeffreyraefan</a> came up with the day to night site design.",
+      'In 24 hours, we raised $6.7 million, with over $1 million going to support the Bail Bond Fund, making this the most commercially successful photograph in history.',
+      "One fun wrinkle in this drop is that the NFT has a built-in toggle to flip between 2 image assets. I spun up a quick site to enable transactions to flip NFTs in a wallet, at <a href='https://gallery.driftershoots.com' target='_blank'>gallery.driftershoots.com</a>.",
     ],
     image: '/portfolio/firstdayout.jpeg',
     contributors: [
