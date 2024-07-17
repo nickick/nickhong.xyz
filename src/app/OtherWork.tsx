b0db593f9087732274be8ef4f79ea51d9900e451
Nick Hong <me@nickhong.xyz>
2024-07-17 13:52:34 -0500

    Fix grammar

--- src/app/OtherWork.tsx
+++ src/app/OtherWork.tsx
@@ -15,24 +15,24 @@ const OtherWork: FC = ({}) => {
         <h2
           className={`flex-[1] w-full ${serif.className} text-3xl text-center md:text-left`}
         >
-          Other Work
+          Other Work <br className="hidden md:block" /> & Skills
         </h2>
         <div className="flex-[3] text-xl gap-8 flex flex-col leading-relaxed">
           <p>
-            Before journeying into Web3, I worked for 10 years as a fullstack
-            Software Engineer / Engineering Manager at a few Web2 companies you
-            might recognize: <Link href="https://medium.com">Medium</Link> ,{" "}
+            Before journeying into Web3, I worked for 10 years at a few Web2
+            companies you might recognize:{" "}
+            <Link href="https://medium.com">Medium</Link> ,{" "}
             <Link href="https://eaze.com">Eaze</Link>,{" "}
             <Link href="https://instacart.com">Instacart</Link>, and{" "}
             <Link href="https://therealreal.com">TheRealReal</Link>.
           </p>
           <p>
-            I&apos;ve become a strong fullstack engineer and how to effectively
-            build products in interdisciplinary teams through these companies. I
-            self-manage well and can manage entire engineering teams through
-            Agile processes to integrate into broader company directives. My
-            technical skills include expertise in React.js, Solidity, Next.js,
-            Express.js, Node.js, AWS, and Ruby on Rails.
+            I&apos;ve become a strong fullstack engineer and learned how to
+            effectively build products in interdisciplinary teams through these
+            companies. I self-manage well and can manage entire engineering
+            teams through agile processes to integrate into broader company
+            directives. My technical skills include expertise in React.js,
+            Solidity, Next.js, Express.js, Node.js, AWS, and Ruby on Rails.
           </p>
           <p>
             For more details, take a look at my{" "}
